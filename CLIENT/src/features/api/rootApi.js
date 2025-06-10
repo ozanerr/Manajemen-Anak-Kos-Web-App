770216ebbb0d8f63222fe33a824faea09b474f95
irdacin <nichodri22@gmail.com>
2025-06-10 19:42:33 +0700

    deploy backend

--- CLIENT/src/features/api/rootApi.js
+++ CLIENT/src/features/api/rootApi.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const rootApi = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5000/api",
-    }),
+        baseUrl: "https://aturin-server.vercel.app/api",
+    }), 
     endpoints: (builder) => ({}),
     tagTypes: ["POST", "COMMENT", "REPLY", "DEADLINE", "FINANCE"],
 });
