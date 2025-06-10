94ed992e9608628071e5f0aa0f9549d0238d9892
fauzan <fauzanaditya18juli@gmail.com>
2025-06-11 00:57:36 +0700

    berhasil deploy railway

--- CLIENT/src/features/api/rootApi.js
+++ CLIENT/src/features/api/rootApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const rootApi = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5000/api",
+        baseUrl: "manajemen-anak-kos-web-app-production.up.railway.app/api",
     }),
     endpoints: (builder) => ({}),
     tagTypes: ["POST", "COMMENT", "REPLY", "DEADLINE", "FINANCE"],
