6d4445df148c99e77c68227189a86fec17612dda
fauzan <fauzanaditya18juli@gmail.com>
2025-06-11 01:05:24 +0700

    api fixing

--- CLIENT/src/features/api/rootApi.js
+++ CLIENT/src/features/api/rootApi.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const rootApi = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "manajemen-anak-kos-web-app-production.up.railway.app/api",
+        baseUrl:
+            "https://manajemen-anak-kos-web-app-production.up.railway.app/api",
     }),
     endpoints: (builder) => ({}),
     tagTypes: ["POST", "COMMENT", "REPLY", "DEADLINE", "FINANCE"],
